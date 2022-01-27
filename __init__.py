3a3b4c4bc91aa5d2a849a75404777174094404ab
Rahul Banerjee <rahul2699@yahoo.com>
2022-01-27 15:57:38 -0500

    CPG Cleaning

--- __init__.py
+++ __init__.py
@@ -62,9 +62,9 @@ def plot_time_series_cpg(path='',outputFileName='cpgData'):
     data[0] = pd.to_datetime(data[0], errors='coerce')
     start = data[0][0]
     data[0] = data.apply(lambda row: (row[0] - start).total_seconds(), axis = 1)
-    data[1] = (data[1]-data[1].min())/(data[1].max()-data[1].min())
     print(data.head())
     data.plot(x=0,y=1,legend=None)
+    data.to_csv('CPG_Data/saminul_extended2_yolo.csv',index=False)
     pyplot.title(outputFileName)
     pyplot.xlabel('Time')
     pyplot.ylabel('CPG Value')
@@ -78,5 +78,5 @@ if __name__ == '__main__':
     name = 'Rahul'
     outputFileName = f'{name}_CPG_Data'
     #ppg_vs_time = collect_data(macAddress,outputFileName=outputFileName)
-    plot_time_series_cpg(path=f'CPG_Data/{outputFileName}.csv',outputFileName = outputFileName)
+    plot_time_series_cpg(path=f'CPG_Data/saminul_extended2.csv',outputFileName = outputFileName)
 
--- signal_preprocessing.py
+++ signal_preprocessing.py
@@ -8,13 +8,13 @@ def normalize(x):
 
 
 
-noisy_signal = genfromtxt('CPG_Data/saminul_extendedCPG.csv', delimiter=',',encoding="utf8")
+noisy_signal = genfromtxt('CPG_Data/saminul_extended2_yolo.csv', delimiter=',',encoding="utf8")
 signal_time_stamp = noisy_signal[:,0]
 noisy_signal = noisy_signal[:,1]
 
 fc_low = 0.1 # cutoff frequency
 fc_high = 18 # cutoff frequency
-fs = 100 # sampling frequency
+fs = 70 # sampling frequency
 wn = [fc_low/(0.5*fs), fc_high/(0.5*fs)]
 sos = signal.butter(4, wn, btype='bandpass', analog=False, output='sos')
 filtered_signal = signal.sosfilt(sos, noisy_signal)
@@ -23,7 +23,7 @@ normalized_signal = np.apply_along_axis(normalize, 0, filtered_signal)
 
 fig = plt.figure()
 ax = plt.axes()
-ax.plot(signal_time_stamp[3000:4000], normalized_signal[3000:4000],label = 'clean');
+ax.plot(signal_time_stamp[2100:2800], normalized_signal[2100:2800],label = 'clean');
 plt.show()
 
 
